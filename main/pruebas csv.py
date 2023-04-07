4572e3ae00a09d176f511a1aa90014b915afa243
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2023-04-07 19:10:02 +0200

    csv

--- main/pruebas csv.py
+++ main/pruebas csv.py
@@ -1,1 +1,18 @@
-print("Hola")
+import csv
+import numpy as np
+from pathlib import Path
+
+json_store_path = str(Path.home()) + r"\PycharmProjects\ia\csv/"
+off = json_store_path + "OFF.csv"
+on = json_store_path + "OFF.csv"
+apagado = json_store_path + "Apagado.csv"
+encendido = json_store_path + "Encendido.csv"
+
+with open(apagado, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+"""matrix = np.loadtxt(apagado, delimiter=',')
+for row in matrix:
+    print(row)"""
