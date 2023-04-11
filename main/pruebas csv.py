b0dea972e5ad40d9344d0ee69f7f9989e78d5c0d
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2023-04-11 16:30:40 +0200

    dcz

--- main/pruebas csv.py
+++ main/pruebas csv.py
@@ -3,14 +3,36 @@ import numpy as np
 from pathlib import Path
 
 json_store_path = str(Path.home()) + r"\PycharmProjects\ia\csv/"
-off = json_store_path + "OFF.csv"
-on = json_store_path + "OFF.csv"
 apagado = json_store_path + "Apagado.csv"
 encendido = json_store_path + "Encendido.csv"
+vfile = json_store_path + "vfile.csv"
+C_ENCENDIDO = 1
+C2_APAGADO = 1
 
-with open(apagado, newline='') as csvfile:
+"""with open(apagado, "r", newline='') as csvfile:
     reader = csv.reader(csvfile)
+with open(encendido, "r", newline='') as csvfile:
+    reader = csv.reader(csvfile)"""
 
-matrix = np.loadtxt(encendido, delimiter=',')
-for row in matrix:
-    print(row)
+matrix_on = np.loadtxt(encendido, delimiter=',')
+matrix_off = np.loadtxt(apagado, delimiter=',')
+
+with open(vfile, "w", newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    b = True
+    for row in matrix_on:
+        if b:
+            b = False
+        else:
+            writer.writerow([row[0], 0])
+
+matrix_v = np.loadtxt(vfile, delimiter=',')
+np.insert(matrix_v, 0, 6.3)
+print(matrix_v)
+
+
+
+end = False
+#while not end:
+
+
